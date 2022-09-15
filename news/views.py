82ed6f56b2a3bce77abef631d07e9ec519288061
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-09-15 15:45:03 -0700

    added news app

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 def news(request):
-    return render(request, 'news.html')
+    newss = News.objects.all().order_by('-date')
+
+    return render(request, 'news.html', {'newss':newss})
