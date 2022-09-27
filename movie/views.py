01e5601ec3f25c52c6d54746487b0447f18fcd3c
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-09-27 14:48:57 -0700

    added delete funtionality, finished project

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,10 @@
+from multiprocessing.sharedctypes import Value
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from .models import Movie, Review
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
@@ -42,6 +44,7 @@ def detail(request, movie_id):
 
     return render(request, 'detail.html', context)
 
+@login_required
 def createreview(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
 
@@ -60,8 +63,24 @@ def createreview(request, movie_id):
             return render(request,'createreview.html',{'form':ReviewForm(),
                                                        'error':'bad data passed in'})
 
+@login_required
 def updatereview(request, review_id):
     review = get_object_or_404(Review,pk=review_id,user=request.user)
+
     if request.method == 'GET':
         form = ReviewForm(instance=review)
-        return (render, 'updatereview.html', {'review':review, 'form':form})
+        return render(request, 'updatereview.html', {'review':review, 'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            return redirect('detail', review.movie.id)
+        except ValueError:
+            return render(request, 'updatereview.html', {'review':review, 'form':form, 'error':'Bad data in form'})
+
+@login_required
+def deletereview(request, review_id):
+    review = get_object_or_404(Review,pk=review_id,user=request.user)
+
+    review.delete()
+    return redirect('detail', review.movie.id)
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -134,4 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'loginaccount'
+
 #eddie_gonzalez::pass
